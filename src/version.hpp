bfdff6d51b23129c5b5759aa35d50a9fe27277c7
- The Aarnold - <mane.imation9@gmail.com>
2023-07-19 17:23:28 +0200

    Configuring project

---
+++ src/version.hpp
@@ -0,0 +1,34 @@
+// version.hpp
+// 19 Jul 2023
+// Gaétan "Gad" Jalin
+// See end of file for complete license notice
+#ifndef BYTA_VERSION_HPP
+#define BYTA_VERSION_HPP
+
+#include <nail/version.hpp>
+
+#define BYTA_VERSION_MAJOR 0
+#define BYTA_VERSION_MINOR 0
+#define BYTA_VERSION_REV 1
+#define BYTA_VERSION_STAGE "a"
+
+#define BYTA_VERSION_STRING nail::makeVersionString(BYTA_VERSION_MAJOR, BYTA_VERSION_MINOR, BYTA_VERSION_REV, BYTA_VERSION_STAGE)
+
+#endif // BYTA_VERSION_HPP
+/**
+ * Copyright (C) 2023 Gaétan Jalin
+ *
+ * This file is part of BYTA.
+ *
+ * BYTA is free software: you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License as published by the Free Software Foundation,
+ * either version 3 of the License, or (at your option) any later version.
+ *
+ * Foobar is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with BYTA.
+ * If not, see <https://www.gnu.org/licenses/>.
+ **/
+
